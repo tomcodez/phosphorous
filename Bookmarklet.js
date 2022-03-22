41a753420811a0fa1e1c087b6e1d4866b77c9924
Michael Saffari <70539144+tomcodez@users.noreply.github.com>
2022-03-22 10:08:14 +0000

    stuff

--- Bookmarklet.js
+++ Bookmarklet.js
@@ -21,7 +21,7 @@ function makeHttpObject() {
   throw new Error("Could not create HTTP request object.");
 };
 var request = makeHttpObject();
-request.open("GET", "https://phosphorous.tomcodez.repl.co/index.html", true);
+request.open("GET", "https://raw.githubusercontent.com/tomcodez/phosphorous/main/index.html", true);
 request.send(null);
 request.onreadystatechange = function () {
 if (request.readyState == 4) 
@@ -32,7 +32,7 @@ if (request.readyState == 4)
   yoiframe.style = "position: fixed; right: 0; bottom: 0; min-width: 100%; min-height: 100%;overflow-x: hidden; z-index:99999999;";    
   document.body.appendChild(yoiframe);    
   let script = document.createElement('script');    
-  script.setAttribute("src", "https://phosphorous.tomcodez.repl.co/script.js");    
+  script.setAttribute("src", "https://raw.githubusercontent.com/tomcodez/phosphorous/main/script.js");    
   script.setAttribute("type", "application/javascript"); 
   document.body.appendChild(script);  
 };
